f019f19207fb81481ec3a7eb6935a1ef7621ed37
Prota <protez95@gmail.com>
2020-03-04 23:14:37 +0100

    Initial commit

---
+++ Assets/BanerAd.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.Advertisements;
+
+public class BanerAd : MonoBehaviour
+{
+    public string gameId = "3495603";
+    public string placementId = "Baner";
+    public bool testMode = true;
+
+    void Start()
+    {
+        Advertisement.Initialize(gameId, testMode);
+        StartCoroutine(ShowBannerWhenReady());
+    }
+
+    IEnumerator ShowBannerWhenReady()
+    {
+        while (!Advertisement.IsReady(placementId))
+        {
+            yield return new WaitForSeconds(0.5f);
+        }
+        Advertisement.Banner.Show(placementId);
+        yield return new WaitForSeconds(15.0f);
+        Advertisement.Banner.Hide();
+    }
+}
---
+++ Assets/VirusSpawner.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VirusSpawner : MonoBehaviour
+{
+    public float maxTime = 1;
+    private float timer = 0;
+    public GameObject virus;
+    public float height;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject newVirus = Instantiate(virus);
+        newVirus.transform.position = transform.position + new Vector3(0, Random.Range(-height, height), 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       if(timer > maxTime)
+        {
+            GameObject newVirus = Instantiate(virus);
+            newVirus.transform.position = transform.position + new Vector3(0, Random.Range(-height, height), 0);
+            Destroy(newVirus, 15);
+            timer = 0;
+        }
+        timer += Time.deltaTime;
+    }
+}
